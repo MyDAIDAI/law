b6db69a9d8b86ae186ada96fd0f49efabb5edf58
daidai <571143755@qq.com>
2017-05-29 18:37:49 +0800

    [+]添加手机端控制器

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -14,14 +14,7 @@ class Index extends Controller
         $this->assign('list',$list);
     	return $this ->fetch();
     }
-    public function indexMobile()
-    {
-        $list = IndexModel::all(function ($query){
-            $query->where('law_id','>',1)->order('law_id','desc')->limit(5);
-        });
-        $this->assign('list',$list);
-        return $this ->fetch();
-    }
+
     public function more(){
         $list = IndexModel::paginate(10);
         $this->assign('list',$list);
@@ -67,4 +60,63 @@ class Index extends Controller
         $this->assign('lawContent',$law->law_content);
         return $this->fetch();
     }
+
+//    手机端控制器
+    public function indexMobile()
+    {
+        $list = IndexModel::all(function ($query){
+            $query->where('law_id','>',1)->order('law_id','desc')->limit(5);
+        });
+        $this->assign('list',$list);
+        return $this ->fetch();
+    }
+
+    public function showMobileAction(){
+        $value = $_POST['value'];
+        $list = IndexModel::where('law_title', 'like', '%'.$value.'%')->paginate(10);
+
+        if ($list) {
+            $this->assign('list',$list);
+            return [
+                'code' => 'ok',
+                'list' => $list,
+                'value' => $value,
+            ];
+        }else{
+            return [
+                'code' => 'error',
+                'msg' => '查询失败',
+            ];
+            
+        }
+        
+    }
+
+    public function showMobile()
+    {
+        return $this->fetch();
+    }
+
+    public function descriptMobile()
+    {
+        $lawName = $_GET['lawName'];
+        $law = IndexModel::get(['law_title'=> $lawName]);
+        $this->assign('value',$lawName);
+        $this->assign('lawName',$law->law_title);
+        $this->assign('lawUnit',$law->law_unit);
+        $this->assign('lawNumber',$law->law_number);
+        $this->assign('lawDate',$law->law_show);
+        $this->assign('lawRelease',$law->law_start);
+        $this->assign('lawEnd',$law->law_end);
+        $this->assign('lawType',$law->law_type);
+        $this->assign('lawContent',$law->law_content);
+        return $this->fetch();
+    }
+
+    public function moreMobile(){
+        $list = IndexModel::paginate(10);
+        $this->assign('list',$list);
+        return $this ->fetch();
+    }
+
 }
