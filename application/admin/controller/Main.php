cab0ecc20b04ec37a71824c8016e2e45212edf12
daidai <571143755@qq.com>
2017-06-04 12:21:12 +0800

    [+]添加管理员管理操作

--- application/admin/controller/Main.php
+++ application/admin/controller/Main.php
@@ -20,51 +20,170 @@ class Main extends Controller
 
     public function user()
     {
-        $adminname = $_GET['adminname'];
-        $this->assign('adminname',$adminname);
         $list = UserModel::all();
-        $this->assign('count',count($list));
-        $this->assign('list',$list);
-        return $this->fetch();
+        $page = $_POST['page'];
+        $limit = $_POST['limit'];
+        $slice = array_slice($list, ($page - 1) * $limit, $limit);
+        if ($list) {
+            return [
+                'code' => 'ok',
+                'list' => $slice,
+                'count' =>  count($list)
+            ];
+        }else{
+            return [
+                'code' => 'error',
+                'msg' => '查询失败',
+            ];
+
+        }
+        
     }
+    public function getUser()
+    {
+        $user = UserModel::get(function ($query){
+            $query -> where('uid',$_POST['user_id']);
+        });
+        if ($user) {
+           
+            return [
+                'code' => 'ok',
+                'data' => $user
+            ];
+        }else{
+            return [
+                'code' => 'error',
+                'msg' => '查找失败!'
+            ];
+        }
+
+    }
+    
+    
     public function law()
     {
-        $adminname = 1;
-        $this->assign('adminname',$adminname);
-        $list = LawModel::paginate(10);
-        $this->assign('count',count($list));
-        $this->assign('list',$list);
-        return $this->fetch();
+        
+        $page = $_POST['page'];
+        $limit = $_POST['limit'];
+        $list = LawModel::all();
+        $slice = array_slice($list, ($page - 1) * $limit, $limit);
+        if ($list) {
+            return [
+                'code' => 'ok',
+                'list' => $slice,
+                'count' =>  count($list)
+            ];
+        }else{
+            return [
+                'code' => 'error',
+                'msg' => '查询失败',
+            ];
+
+        }
     }
     public function addUser()
     {
-        $adminname = $_GET['adminname'];
-        $this->assign('adminname',$adminname);
-        return $this->fetch();
+        $user = new UserModel([
+            'username' => $_POST['user_name'],
+            'password' => md5($_POST['user_pass']),
+            'email' => $_POST['user_email'],
+            'qq' => $_POST['user_qq'],
+            'mobile' => $_POST['user_mobile']
+        ]);
+        $user -> save();
+        if ($user) {
+            return [
+                'code' => 'ok',
+                'msg'=> '修改成功!'
+            ];
+        }else{
+            return [
+                'code' => 'error',
+                'msg' => '修改失败!'
+            ];
+        }
+
     }
-    public function deleteUser($value='')
+
+    public function updateUser()
     {
         $user = UserModel::get(function ($query){
-            $query -> where('user_name',$_GET['username']);
+            $query -> where('uid',$_POST['user_id']);
+        });
+
+        if ($user) {
+            $user -> save([
+                    'username' => $_POST['user_name'],
+                    'password' => $_POST['user_pass'],
+                    'email' => $_POST['user_email'],
+                    'qq' => $_POST['user_qq'],
+                    'mobile' => $_POST['user_mobile']
+                ],['uid',$_POST['user_id']]);
+            return [
+                'code' => 'ok',
+                'msg'=> '修改成功!'
+            ];
+        }else{
+            return [
+                'code' => 'error',
+                'msg' => '修改失败!'
+            ];
+        }
+
+    }
+    public function deleteUser()
+    {
+        $user = UserModel::get(function ($query){
+            $query -> where('uid',$_POST['user_id']);
         });
         if ($user) {
             $user -> delete();
-            echo 'yes';
+            return [
+                'code' => 'ok',
+                'msg' => '删除成功!'
+            ];
         }else{
-            echo 'no';
+            return [
+                'code' => 'error',
+                'msg' => '删除失败!'
+            ];
         }
     }
     public function deleteLaw()
     {
        $law = LawModel::get(function ($query){
-            $query -> where('law_title',$_GET['lawname']);
-        });
+            $query -> where('law_id',$_POST['law_id']);
+       });
+        
+        
         if ($law) {
             $law -> delete();
-            echo 'yes';
+            return [
+                'code' => 'ok',
+                'msg' => '删除成功!'
+            ];
         }else{
-            echo 'no';
+            return [
+                'code' => 'error',
+                'msg' => '删除失败!'
+            ];
         }
+    }
+
+    public function descript()
+    {
+        $lawName = $_GET['id'];
+        $law = LawModel::get(['law_id'=> $lawName]);
+
+        $this->assign('lawName',$law->law_title);
+        $this->assign('lawUnit',$law->law_unit);
+        $this->assign('lawNumber',$law->law_number);
+        $this->assign('lawDate',$law->law_show);
+        $this->assign('lawRelease',$law->law_start);
+        $this->assign('lawEnd',$law->law_end);
+        $this->assign('lawType',$law->law_type);
+        $this->assign('lawContent',$law->law_content);
+        return $this->fetch();
     }
     public function adminName()
     {
